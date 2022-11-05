aa88d2fb6c393af288fea96e9500d741c8fb3cc1
Prog. Shahad <92324768+progShahad@users.noreply.github.com>
2022-11-05 18:24:21 +0300

    Update Team.java

--- src/main/Team.java
+++ src/main/Team.java
@@ -4,18 +4,12 @@ import java.util.ArrayList;
 public class Team {
     //Data fields
     public String teamName;
-    public int numberOfMembers;
     public ArrayList<String> membersNames;
     
     //Constructor
-    Team(){
-        
-    }
-    Team(String teamName, int numberOfMembers, ArrayList<String> membersNames){
+    Team(String teamName, ArrayList<String> membersNames){
         this.teamName = teamName;
-        this.numberOfMembers = numberOfMembers;
         this.membersNames = membersNames;
     }
-    //Other methods
     
 } //end of Team class
